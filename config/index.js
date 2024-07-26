95f03b8a488d4d9fa638edfaf4b6c968b090c982
Kshitij Satpathy <akuma10901@gmail.com>
2024-07-26 16:46:17 +0530

    dev: backend

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     ORIGIN: process.env.CORS_ORIGIN,
     PORT: process.env.PORT || 3000,
-    DB_URL: process.env.MONGO_URI || "mongodb://localhost:27017/srimandir",
+    DB_URL: process.env.DB_URI || "mongodb://localhost:27017/srimandir",
     JWT_SECRET: process.env.JWT_SECRET || "secret",
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
 };
